c2d82c2991ef36ad62bff5960f768bfb13bc947d
JosieMCantu <josiex2@gmail.com>
2021-04-09 16:45:24 -0700

    adds get route for all actors

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,10 @@
 
 const db = require('../lib/utils/db');
 const faker = require('faker');
-const setup = require('../data/setup');
+
 const request = require('supertest');
 const app = require('../lib/app');
+const Actor = require('../lib/models/Actor');
 
 describe('ripe-banana routes', () => {
   beforeEach(() => {
@@ -24,6 +25,39 @@ describe('ripe-banana routes', () => {
       dob: expect.any(String),
       pob: expect.any(String)
     })
+  })
+
+  it('GET returns all actors', async () => {
+    await Actor.bulkCreate([
+      {
+      name: `${faker.name.findName()}`,
+      dob: `${faker.date.past()}`,
+      pob: `${faker.address.country()}`
+      },
+      {
+      name: `${faker.name.findName()}`,
+      dob: `${faker.date.past()}`,
+      pob: `${faker.address.country()}`
+      }])
+
+    return request(app)
+      .get('/api/v1/actors')
+      .then((res) => {
+        expect(res.body).toEqual([
+          {
+          id: expect.any(Number),
+          name: expect.any(String),
+          dob: expect.any(String),
+          pob: expect.any(String)
+        },{
+          id: expect.any(Number),
+          name: expect.any(String),
+          dob: expect.any(String),
+          pob: expect.any(String)
+        }])
+      })
+
+    
   })
 
 });
