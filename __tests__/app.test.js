160c716f5b07a679ed4a1bcdaa3dcee4843b5fe9
JosieMCantu <josiex2@gmail.com>
2021-04-09 15:58:08 -0700

    post actor test fail

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,28 @@
-const pool = require('../lib/utils/pool');
+
+const db = require('../lib/utils/db');
+const faker = require('faker');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
 describe('ripe-banana routes', () => {
   beforeEach(() => {
-    return setup(pool);
+    return db.sync({force:true});
   });
+
+  it('POST creates a new actor', async () => {
+    const res = await request(app)
+    .post('/api/v1/actors/create')
+    .send({
+      name: `${faker.name.findName()}`,
+      dob: `${faker.date.past()}`,
+      pob: `${faker.address.country()}`
+    })
+    expect(res.body).toEqual({
+      name: expect.any(String),
+      dob: expect.any(Date),
+      pob: expect.any(String)
+    })
+  })
+  
 });
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+
 const app = require('./lib/app');
-const pool = require('./lib/utils/pool');
+
 
 const PORT = process.env.PORT || 7890;
 
@@ -10,5 +11,5 @@ app.listen(PORT, () => {
 
 process.on('exit', () => {
   console.log('Goodbye!');
-  pool.end();
+  
 });
