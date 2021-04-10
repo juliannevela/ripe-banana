2591e573207f1e5cce49b7bafc654ba5c0fc1f0c
Cherno Badjan <badjancherno@gmail.com>
2021-04-10 10:18:18 -0700

    Reviewer POST test RED

--- __tests__/reviewer.test.js
+++ __tests__/reviewer.test.js
@@ -2,9 +2,24 @@ const db = require('../lib/utils/db');
 const faker = require('faker');
 const request = require('supertest');
 const app = require('../lib/app');
+const Reviewer = require('../lib/models/Reviewer')
 
-describe.skip('ripe-banana routes', () => {
-	it('should', async () => {
-		expect('this').toEqual('this');
+describe('ripe-banana routes', () => {
+	beforeEach(() => {
+		return db.sync({force:true});
+	});
+	it('POST creates a new Reviewer', async () => {
+		const res = await request(app)
+		.post('/api/v1/reviewers/create')
+		.send({
+			name: `${faker.name.findName()}`,
+			company:`${faker.company.companyName()}`
+
+		})
+		expect(res.body).toEqual({
+			id: expect.any(Number),
+			name: expect.any(String),
+			company: expect.any(String),
+				});
 	});
 });
