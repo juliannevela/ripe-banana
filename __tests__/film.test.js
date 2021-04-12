22af9e9691f55ea172e539422b7db2b52942d338
Cherno Badjan <badjancherno@gmail.com>
2021-04-12 12:05:56 -0700

    film POST test RED

--- __tests__/film.test.js
+++ __tests__/film.test.js
@@ -3,8 +3,26 @@ const faker = require('faker');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe.skip('ripe-banana routes', () => {
-	it('should', async () => {
-		expect('this').toEqual('this');
+describe('ripe-banana routes', () => {
+	it('POST should creates a FILM', async () => {
+		const studio = {id: 1, name:'Studio', city:'Portland', state: 'Oregon', country: ' U.S.A'};
+		const actor = {id: 1, name: 'Will Smith', dob: '01/01/91', pob: 'Portland'}
+		const res = await request(app)
+		.post('/api/v1/films/create')
+		.send({
+			title: `${faker.lorem.words(3)}`,
+			studio: studio.id,
+			released: '2005',
+			cast:[{role: `${faker.name.findName()}`, actor: actor.id}]
+		})
+		expect(res.body).toEqual({
+			id: expect.any(Number),
+			title: expect.any(String),
+			studio: studio.id,
+			released: '2005',
+			cast: [{role: expect.any(String),
+			actor: actor.id}]
+
+		});
 	});
 });
--- __tests__/review.test.js
+++ __tests__/review.test.js
@@ -4,7 +4,22 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe.skip('ripe-banana routes', () => {
-	it('should', async () => {
+	it('should GET top 100 rated reviews', async () => {
+		// const reviews = await Promise.all(
+		// 	Review.bulkCreate(
+		// 		[...Array(200)]
+		// 			.map((review, i) => (
+		// 					{
+		// 						id: i+1,
+		// 						rating:`${faker.datatype.number({'min':1, 'max':5})}`, 
+		// 						review:`${faker.lorem.paragraph( 3)}`,
+		// 						film_id:`${faker.datatype.number({'min':1, 'max':200})}`
+
+		// 					}
+		// 			)
+		// 		)
+		// 	)
+		// );
 		expect('this').toEqual('this');
 	});
 });
