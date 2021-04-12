6c57fca9839c2610d771da4222eb331b5a575203
JosieMCantu <josiex2@gmail.com>
2021-04-12 15:46:24 -0700

    adds GET films route RED

--- __tests__/film.test.js
+++ __tests__/film.test.js
@@ -3,11 +3,41 @@ const faker = require('faker');
 const request = require('supertest');
 const app = require('../lib/app');
 const Film = require('../lib/models/Film');
+const Studio = require('../lib/models/Studio');
+
+const seedStudio = {
+  name: `${faker.company.companyName()}`,
+  city: `${faker.address.city()}`,
+  state: `${faker.address.state(true)}`,
+  country: `${faker.address.country()}`,
+};
+
+const seedStudioArray = [
+  {
+    name: `${faker.company.companyName()}`,
+    city: `${faker.address.city()}`,
+    state: `${faker.address.state(true)}`,
+    country: `${faker.address.country()}`,
+  },
+  {
+    name: `${faker.company.companyName()}`,
+    city: `${faker.address.city()}`,
+    state: `${faker.address.state(true)}`,
+    country: `${faker.address.country()}`,
+  },
+  {
+    name: `${faker.company.companyName()}`,
+    city: `${faker.address.city()}`,
+    state: `${faker.address.state(true)}`,
+    country: `${faker.address.country()}`,
+  },
+];
 
 describe('ripe-banana routes', () => {
   beforeEach(() => {
     return db.sync({ force: true });
   });
+
   it('POST should creates a FILM', async () => {
     const res = await Film.create({
       title: `${faker.lorem.words(3)}`,
@@ -19,4 +49,26 @@ describe('ripe-banana routes', () => {
       released: 2005,
     });
   });
+
+  it('GET should get a list of films', async () => {
+    const studio = await Studio.create(seedStudio);
+
+    Film.create({
+      title: `${faker.lorem.words(3)}`,
+      released: 2005,
+
+    });
+    const res = await request(app)
+    .get('/api/v1/films')
+
+    expect(res.body).toEqual({
+      id: expect.any(Number),
+      title: expect.any(String),
+      studio: {
+        id: studio.dataValues.id,
+        name: studio.dataValues.name
+      }
+
+    })
+  })
 });
